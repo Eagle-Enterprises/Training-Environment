df52ab6321c881be740b336ea3e4fddf590392e9
maplebeans1 <svanchiro1@gmail.com>
2024-05-16 18:28:25 -0700

    created 2D marker that appears on minimap

---
+++ AirSim/Unreal/Environments/Blocks/Source/Blocks/SpriteMySceneCaptureComponent2D.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneCaptureComponent2D.h"
+#include "SpriteMySceneCaptureComponent2D.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLOCKS_API USpriteMySceneCaptureComponent2D : public USceneCaptureComponent2D
+{
+	GENERATED_BODY()
+	
+};
